7fdb15d3ef71badb6d6d36f51d56bd0cadebee92
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-10-25 20:09:04 +1100

    Comment out intentionally failing code

--- Assets/Behaviours/BlobBehaviour.cs
+++ Assets/Behaviours/BlobBehaviour.cs
@@ -5,7 +5,7 @@ public class BlobBehaviour : BaseBehaviour
     protected override void Inject(IKernel kernel)
     {
         // This will intentionally fail.
-        kernel.Get<IHasComponentOnSameGameObject<PlayerBehaviour>>();
+        //kernel.Get<IHasComponentOnSameGameObject<PlayerBehaviour>>();
     }
 
     protected override void AwakeAfterInjection()
